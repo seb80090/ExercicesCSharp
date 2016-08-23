521597d6d0133481843c0eaf764c79784a0cdb88
seb80090 <ssb.oger@gmail.com>
2016-08-23 16:40:19 +0200

    modif

--- Tableaux exercice 2/Program.cs
+++ Tableaux exercice 2/Program.cs
@@ -8,11 +8,15 @@ namespace Tableaux_exercice_2
 {
     class Program
     {
-        static void Main()
+        static void Main();
+        
+        Console.WriteLine("***Tableaux exercice 2 v1.0 *** 23/08/2016");
+
         {
             
 
-        }       
+        }
+        //fonction GetInteger pour lire un entier au clavier,        
         static int GetInteger(string message)
         {
             Console.WriteLine(message);
@@ -21,24 +25,38 @@ namespace Tableaux_exercice_2
 
         }
 
-        static void SaisieTab() {
+            //une fonction InitTab pour créer et initialiser l’instance de tableau 
+            static void InitTab()
+        {
+
 
             int[] tab = new int[10];
-            
-            
+
+            //une fonction InitTab pour créer et initialiser l’instance de tableau de type entier : le nombre de postes souhaité sera entré au clavier
+
             Console.WriteLine("Entrez la taille du tableau");
             int taille = Int32.Parse(Console.ReadLine());
 
             for (int i = 0; i < taille; i++)
+
+                return taille;
             {
                 Console.WriteLine("Entrez un nombre");
                 tab[i] = Int32.Parse(Console.ReadLine());
             }
-
+            
             for (int i = 0; i < taille; i++)
             {
-                Console.Write(tab[i] + " ");
+                Console.Write(tab[i] + " ");          
             }
+
+            //une fonction AfficheTab pour afficher tous les postes du tableau,
+
+            static void AfficheTab();
+
+            AfficheTab =Console.WriteLine( );
+
+
             Console.WriteLine("Appuyez sur une touche pour terminer");
             Console.ReadLine();
            }
